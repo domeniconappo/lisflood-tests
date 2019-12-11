89896e8a6c4d8ee7c5158eb3672c41f04bd2269c
Domenico Nappo <domenico.nappo@gmail.com>
2019-12-11 10:05:38 +0100

    add 6hoyrly settings. Adjusted testing paths for newer lisflood versions

--- listests/test_long_efas_run.py
+++ listests/test_long_efas_run.py
@@ -1,7 +1,5 @@
 import os
-import sys
 import uuid
-import subprocess
 from pathlib import Path
 
 from bs4 import BeautifulSoup
@@ -21,13 +19,13 @@ Start Date - End Date:  2015-01-06 06:00:00  -  2017-06-30 06:00:00
 
 How to run:
 
-pytest test_long_efas_run.py -s \
-  -L /workarea/lisflood_versions/1_e5eb9f03/lisf1.py \ # Path to lisf1.py main script
-  -R /workarea/EFAS/ \ # Path to root (PathRoot in settings file)
-  -M /workarea/EFAS/EFAS_forcings/ \  # Path to meteo forcings
-  -O /workarea/lf_results/1_e5eb9f03/ \  # Path to output folder
-  -P /workarea/virtualenvs/lisflood27/bin/python \  # Path to python binary
-  -I /workarea/lf_results/reference/EFAS/InitSafe/ \  # Path to init folder
+pytest test_long_efas_run.py -s
+  -L /workarea/lisflood_versions/1_e5eb9f03/lisf1.py  # Path to lisf1.py main script
+  -R /workarea/EFAS/  # Path to root (PathRoot in settings file)
+  -M /workarea/EFAS/EFAS_forcings/   # Path to meteo forcings
+  -O /workarea/lf_results/1_e5eb9f03/   # Path to output folder
+  -P /workarea/virtualenvs/lisflood27/bin/python   # Path to python binary
+  -I /workarea/lf_results/reference/EFAS/InitSafe/   # Path to init folder
   -X /workarea/lf_results/reference/EFAS/out_daily  # Path to reference data (Oracle data)
 
 """
@@ -40,7 +38,7 @@ class TestLongRun:
         # run lisflood
         pybin = cls.options['python']
         lisflood_py = cls.options['lisflood']
-        compile_krw = cls.options['lisflood'].parent.joinpath('hydrological_modules/compile_kinematic_wave_parallel_tools.py')
+        compile_krw = lisflood_py.parent.joinpath('hydrological_modules/compile_kinematic_wave_parallel_tools.py') if lisflood_py else None
         pathout = cls.options['pathout']
         cls.settings_xml = cls.get_settings()
         cell = cls.settings_xml.select('lfuser textvar[name="MaskMap"]')[0]
@@ -60,13 +58,13 @@ class TestLongRun:
             return
 
         # Compile kinematic wave
-        kw_dir = lisflood_py.parent.joinpath('hydrological_modules/')
-        lis_dir = lisflood_py.parent.parent
+        kw_dir = lisflood_py.parent.joinpath('lisflood/hydrological_modules/')
+        lis_dir = lisflood_py.parent
         compile_cmd = ' '.join((f'cd {kw_dir} &&', pybin.as_posix(), compile_krw.name, 'build_ext', '--inplace'))
         run_command(compile_cmd)
 
         logger.info(' ============================== START OF LISFLOOD OUTPUT ============================== ')
-        lisflood_cmd = ' '.join((f'cd {lis_dir} &&', pybin.as_posix(), 'lisflood/lisf1.py', cls.settings_filepath.as_posix()))
+        lisflood_cmd = ' '.join((f'cd {lis_dir} &&', pybin.as_posix(), 'lisf1.py', cls.settings_filepath.as_posix()))
         run_command(lisflood_cmd)
         logger.info(' ============================== END OF LISFLOOD OUTPUT ================================ ')
 
@@ -91,12 +89,14 @@ class TestLongRun:
 
     def test_rep_maps(self):
         # check all nc. maps in output folder
+        logger.info(' ============================== START NETCDF TESTS ============================== ')
         comparator = NetCDFComparator(self.mask_map)
         diffs = comparator.compare_dirs(self.options['reference'], self.options['pathout'])
         assert not diffs
 
     def test_tss(self):
         # check all TSS in output folder
+        logger.info(' ============================== START TSS TESTS ============================== ')
         comparator = TSSComparator()
         diffs = comparator.compare_dirs(self.options['reference'], self.options['pathout'])
         assert not diffs
